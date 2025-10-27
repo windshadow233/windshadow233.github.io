31352d74d36102748c6256410f42224f421783f8
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2025-10-27 15:52:42 +0800

    refactor(blog): 重构 link.js 中的 URL 处理逻辑

--- themes/butterfly/scripts/tag/link.js
+++ themes/butterfly/scripts/tag/link.js
@@ -4,12 +4,27 @@ function link(args) {
     let sitename = args[1];
     let link = args[2];
     // let urlNoProtocol = link.replace(/^https?\:\/\//i, "");
-    let hostname = new URL(link).hostname;
-    let imgUrl = "https://logo.clearbit.com/" + hostname + "?size=256";
+    let hostname;
+    try {
+        hostname = new URL(link).hostname;
+    }
+    catch (e) {
+        hostname = '';
+    }
+    // check if the hostname ends with .fyz666.xyz
+    let tip, imgUrl;
+    if (!hostname) {
+        tip = '引用本站资源，请放心访问';
+        imgUrl = '/images/avatar.webp';
+    } else {
+        tip = '引用站外地址，不保证站点的可用性和安全性';
+        imgUrl = "https://logo.clearbit.com/" + hostname + "?size=256";
+    }
+    
     let fallbackImgUrl = "/images/default-link.png";
 
     return `<a class="tag-Link" target="_blank" href="${link}">
-    <div class="tag-link-tips">引用站外地址，不保证站点的可用性和安全性</div>
+    <div class="tag-link-tips">${tip}</div>
     <div class="tag-link-bottom">
         <div class="tag-link-left">
             <img src="${imgUrl}" onerror="this.onerror=null;this.src='${fallbackImgUrl}';" />
