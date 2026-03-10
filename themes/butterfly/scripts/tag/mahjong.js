082f7323c14b4e0e4e92a0327014a5072e5975db
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2026-03-10 23:34:03 +0800

    更新图片链接

--- themes/butterfly/scripts/tag/mahjong.js
+++ themes/butterfly/scripts/tag/mahjong.js
@@ -7,27 +7,55 @@
 
 const BLANK = "<img class=\"blank-tile\" src=\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg'/%3E\">"
 
-const loadPngs = () => {
-  const BASE = 'https://fastly.jsdelivr.net/gh/windshadow233/Riichi-Calculator/static/png'
-  const pngs = {}
-  for (let i = 0; i < 9; i++) pngs[i] = `${BASE}/${i + 1}man.png`
-  pngs[-1] = `${BASE}/akaman.png`
-  for (let i = 10; i < 19; i++) pngs[i] = `${BASE}/${i - 9}pin.png`
-  pngs[9] = `${BASE}/akapin.png`
-  for (let i = 20; i < 29; i++) pngs[i] = `${BASE}/${i - 19}sou.png`
-  pngs[19] = `${BASE}/akasou.png`
-  pngs[30] = `${BASE}/tan.png`
-  pngs[40] = `${BASE}/nan.png`
-  pngs[50] = `${BASE}/xia.png`
-  pngs[60] = `${BASE}/pei.png`
-  pngs[70] = `${BASE}/haku.png`
-  pngs[80] = `${BASE}/hatsu.png`
-  pngs[90] = `${BASE}/chun.png`
-  pngs[-2] = `${BASE}/back.png`
-  return pngs
-}
+const pngs = {
+  // 万子
+  0: "https://cdn4.winhlb.com/2026/03/07/69ab03abd718c.png", // 1万
+  1: "https://cdn4.winhlb.com/2026/03/07/69ab03ac073cc.png", // 2万
+  2: "https://cdn4.winhlb.com/2026/03/07/69ab03ac1c0a4.png", // 3万
+  3: "https://cdn4.winhlb.com/2026/03/07/69ab03abe3b85.png", // 4万
+  4: "https://cdn4.winhlb.com/2026/03/07/69ab03abef2ef.png", // 5万
+  5: "https://cdn4.winhlb.com/2026/03/07/69ab03bd8bc5e.png", // 6万
+  6: "https://cdn4.winhlb.com/2026/03/07/69ab03bd9c9e7.png", // 7万
+  7: "https://cdn4.winhlb.com/2026/03/07/69ab03bde1112.png", // 8万
+  8: "https://cdn4.winhlb.com/2026/03/07/69ab03bde23ec.png", // 9万
+  [-1]: "https://cdn4.winhlb.com/2026/03/07/69ab03bd8730f.png", // 赤万子
 
-const pngs = loadPngs()
+  // 饼子
+  10: "https://cdn4.winhlb.com/2026/03/07/69ab03ea7d0c6.png", // 1饼
+  11: "https://cdn4.winhlb.com/2026/03/07/69ab03ea9578c.png", // 2饼
+  12: "https://cdn4.winhlb.com/2026/03/07/69ab04348ab2a.png", // 3饼
+  13: "https://cdn4.winhlb.com/2026/03/07/69ab0411d3c98.png", // 4饼
+  14: "https://cdn4.winhlb.com/2026/03/07/69ab0411b9d18.png", // 5饼
+  15: "https://cdn4.winhlb.com/2026/03/07/69ab04481d697.png", // 6饼
+  16: "https://cdn4.winhlb.com/2026/03/07/69ab045fdbb0b.png", // 7饼
+  17: "https://cdn4.winhlb.com/2026/03/07/69ab045fd5e01.png", // 8饼
+  18: "https://cdn4.winhlb.com/2026/03/07/69ab044822edd.png", // 9饼
+  9: "https://cdn4.winhlb.com/2026/03/07/69ab04765b626.png", // 赤饼子
+
+  // 索子
+  20: "https://cdn4.winhlb.com/2026/03/07/69ab09e36c67e.png", // 1索
+  21: "https://cdn4.winhlb.com/2026/03/07/69ab09c125df8.png", // 2索
+  22: "https://cdn4.winhlb.com/2026/03/07/69ab09c14432e.png", // 3索
+  23: "https://cdn4.winhlb.com/2026/03/07/69ab09c139e08.png", // 4索
+  24: "https://cdn4.winhlb.com/2026/03/07/69ab09e36a857.png", // 5索
+  25: "https://cdn4.winhlb.com/2026/03/07/69ab0a04a8f52.png", // 6索
+  26: "https://cdn4.winhlb.com/2026/03/07/69ab0a04a8fc4.png", // 7索
+  27: "https://cdn4.winhlb.com/2026/03/07/69ab0a2a4a3e1.png", // 8索
+  28: "https://cdn4.winhlb.com/2026/03/07/69ab0a1e7a28d.png", // 9索
+  19: "https://cdn4.winhlb.com/2026/03/07/69ab0a1e7e544.png", // 赤索子
+
+  // 字牌
+  30: "https://cdn4.winhlb.com/2026/03/07/69ab0a46d45e7.png", // 东
+  40: "https://cdn4.winhlb.com/2026/03/07/69ab0a4c516ff.png", // 南
+  50: "https://cdn4.winhlb.com/2026/03/07/69ab0a5157c13.png", // 西
+  60: "https://cdn4.winhlb.com/2026/03/07/69ab0a4a540d0.png", // 北
+  70: "https://cdn4.winhlb.com/2026/03/07/69ab0a6130a8d.png", // 白
+  80: "https://cdn4.winhlb.com/2026/03/07/69ab0a638393a.png", // 发
+  90: "https://cdn4.winhlb.com/2026/03/07/69ab0a5d34cca.png", // 中
+
+  // 特殊牌
+  [-2]: "https://cdn4.winhlb.com/2026/03/07/69ab0a5f26507.png", // 背面
+}
 
 function _str2id (tiles, use_aka = true) {
   let stack = ''
