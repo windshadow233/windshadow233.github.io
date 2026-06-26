71e6bd953be5c68efc0441694b46ee052cb0f17d
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2026-06-27 00:48:35 +0800

    update post

--- themes/butterfly/scripts/tag/mahjong.js
+++ themes/butterfly/scripts/tag/mahjong.js
@@ -7,54 +7,63 @@
 
 const BLANK = "<img class=\"blank-tile\" src=\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg'/%3E\">"
 
-const pngs = {
+const PNG_KEYS = [
   // 万子
-  0: "https://cdn4.winhlb.com/2026/03/07/69ab03abd718c.png", // 1万
-  1: "https://cdn4.winhlb.com/2026/03/07/69ab03ac073cc.png", // 2万
-  2: "https://cdn4.winhlb.com/2026/03/07/69ab03ac1c0a4.png", // 3万
-  3: "https://cdn4.winhlb.com/2026/03/07/69ab03abe3b85.png", // 4万
-  4: "https://cdn4.winhlb.com/2026/03/07/69ab03abef2ef.png", // 5万
-  5: "https://cdn4.winhlb.com/2026/03/07/69ab03bd8bc5e.png", // 6万
-  6: "https://cdn4.winhlb.com/2026/03/07/69ab03bd9c9e7.png", // 7万
-  7: "https://cdn4.winhlb.com/2026/03/07/69ab03bde1112.png", // 8万
-  8: "https://cdn4.winhlb.com/2026/03/07/69ab03bde23ec.png", // 9万
-  [-1]: "https://cdn4.winhlb.com/2026/03/07/69ab03bd8730f.png", // 赤万子
-
+  0,1,2,3,4,5,6,7,8,-1,
   // 饼子
-  10: "https://cdn4.winhlb.com/2026/03/07/69ab03ea7d0c6.png", // 1饼
-  11: "https://cdn4.winhlb.com/2026/03/07/69ab03ea9578c.png", // 2饼
-  12: "https://cdn4.winhlb.com/2026/03/07/69ab04348ab2a.png", // 3饼
-  13: "https://cdn4.winhlb.com/2026/03/07/69ab0411d3c98.png", // 4饼
-  14: "https://cdn4.winhlb.com/2026/03/07/69ab0411b9d18.png", // 5饼
-  15: "https://cdn4.winhlb.com/2026/03/07/69ab04481d697.png", // 6饼
-  16: "https://cdn4.winhlb.com/2026/03/07/69ab045fdbb0b.png", // 7饼
-  17: "https://cdn4.winhlb.com/2026/03/07/69ab045fd5e01.png", // 8饼
-  18: "https://cdn4.winhlb.com/2026/03/07/69ab044822edd.png", // 9饼
-  9: "https://cdn4.winhlb.com/2026/03/07/69ab04765b626.png", // 赤饼子
-
+  10,11,12,13,14,15,16,17,18,9,
   // 索子
-  20: "https://cdn4.winhlb.com/2026/03/07/69ab09e36c67e.png", // 1索
-  21: "https://cdn4.winhlb.com/2026/03/07/69ab09c125df8.png", // 2索
-  22: "https://cdn4.winhlb.com/2026/03/07/69ab09c14432e.png", // 3索
-  23: "https://cdn4.winhlb.com/2026/03/07/69ab09c139e08.png", // 4索
-  24: "https://cdn4.winhlb.com/2026/03/07/69ab09e36a857.png", // 5索
-  25: "https://cdn4.winhlb.com/2026/03/07/69ab0a04a8f52.png", // 6索
-  26: "https://cdn4.winhlb.com/2026/03/07/69ab0a04a8fc4.png", // 7索
-  27: "https://cdn4.winhlb.com/2026/03/07/69ab0a2a4a3e1.png", // 8索
-  28: "https://cdn4.winhlb.com/2026/03/07/69ab0a1e7a28d.png", // 9索
-  19: "https://cdn4.winhlb.com/2026/03/07/69ab0a1e7e544.png", // 赤索子
-
+  20,21,22,23,24,25,26,27,28,19,
   // 字牌
-  30: "https://cdn4.winhlb.com/2026/03/07/69ab0a46d45e7.png", // 东
-  40: "https://cdn4.winhlb.com/2026/03/07/69ab0a4c516ff.png", // 南
-  50: "https://cdn4.winhlb.com/2026/03/07/69ab0a5157c13.png", // 西
-  60: "https://cdn4.winhlb.com/2026/03/07/69ab0a4a540d0.png", // 北
-  70: "https://cdn4.winhlb.com/2026/03/07/69ab0a6130a8d.png", // 白
-  80: "https://cdn4.winhlb.com/2026/03/07/69ab0a638393a.png", // 发
-  90: "https://cdn4.winhlb.com/2026/03/07/69ab0a5d34cca.png", // 中
+  30,40,50,60,70,80,90,
+  // 特殊
+  -2
+]
 
-  // 特殊牌
-  [-2]: "https://cdn4.winhlb.com/2026/03/07/69ab0a5f26507.png", // 背面
+const imageURLs = 
+`https://pic.sl.al/gdrive/pic/2026-04-08/fileid_13Ml7kUgPHkVnlIubvQZWFnGxxxqqa0zy_1man.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1OzKuJ1axvOfj2Yw6E_o_V3NJxxM70XYf_2man.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1gxUeKE9JIHEL3_MuR1fydbGsySiLggqH_3man.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1V82cfeDAiXPvzcpFPEojV92PjsdTogvc_4man.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1WU2lJRk6MKjJ-PRiZxEXZbNLzO1OgyNm_5man.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1nbySCnS38VrJuFSb83QmiAeJX973w0_F_6man.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1Fy-ARC3N_5Lccv-AfKBDUR8VNqC1KJhc_7man.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1Ey9qrF-MoijCsFcy2Sjo1XS5Mv7Rig50_8man.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1J_k1UGl-3CWn2_5E5FhlOy_mI3Y13q_I_9man.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1n2MXe5eWS0JsjL47inRMI1ZCFpNmMRdJ_akaman.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1K4c__x2sKI4Sm8lpqpH9u3QKHy7Yy9df_1pin.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1Z7703uYgHoZZVbz3dupS-5UV8HkYILvd_2pin.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1-OFMkTJYp60LokrIW1MIxtWQ6Q8ODuIw_3pin.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1-Mk4KXmiIZzKxLKP6-sxAaCafqj-4rvJ_4pin.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1QhZjQj9zVBhK43IN0YkIKgvBcZOalVLR_5pin.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1C3KyJuRmpSmy9iZwcUdEy77K6DW78qGc_6pin.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_15p9dah2fdC4q_x8-g1jJHv05Y7NwhiBm_7pin.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1e-FBond5JUVbpVHPiGG2f_wpfRzY80cX_8pin.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1a8BEkbgwHiD7EiRO19ofmjRg5XE2nHT1_9pin.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1rhU7abbsMSYoSNPxNz6af-O1F88vcBnR_akapin.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_13RWEHTtPfjPMVBK9ZPInZFFLVAflgE6__1sou.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_19R75dKU2nh6iQD4wqI0sBSAbOW19de4u_2sou.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1y7rsvWXcMpnvJZs6JofvDSMQRMX1mnB-_3sou.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1F9d3EerifcGczlU6HVhxK4RrDzNaUlPV_4sou.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1XzCguaSswig-5RoLGBk-Ics0r_RaRHpU_5sou.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1XLUj_SKlJ3_gdOsdkmAG9_kyapiEAB9g_6sou.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1imCE5UTJgualKwAlbAJy0O4En17fqRt-_7sou.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1ydkiYAXLL4FAjeO8CKNOIKges6ryU8oa_8sou.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1WE6PyQKYTGANaHIicZnIX-9jpnieT7AY_9sou.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1huY5q_pAbsmQCPB3zajyWdrWyxB8CSAT_akasou.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1b75TcEdh1xo8nrkUsOFt4wsJwO6ngOHF_tan.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_17VGbN60ebC6i4ioqW549thsxaxZ-cJsn_nan.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1iAKx6sKyBOAN217FKH-y1b6VyygPSMFo_xia.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1N4k1CdDfo_7xOw12iblPKISsB-cN9C83_pei.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1uXNsAiK0uGTIE8ckYXHF4_1sbrd_wpZ7_haku.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1CMyQL8LizayKDJd2k1FJB61O2ZxeKL1Y_hatsu.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_1mi0PEvNCGVQcY0-A5GrkkJnDCb5yX8NZ_chun.png
+https://pic.sl.al/gdrive/pic/2026-04-08/fileid_11DeV_Dw3m2JGke34Yzyuneia7mA7036w_back.png`.split('\n')
+
+const pngs = {}
+for (let i = 0; i < imageURLs.length; i++) {
+  let url = imageURLs[i]
+  pngs[PNG_KEYS[i]] = url
 }
 
 function _str2id (tiles, use_aka = true) {
